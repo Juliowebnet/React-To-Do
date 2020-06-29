3c97fbb66b00da4f9889358aa84c88727f1fe921
Julio Martínez <info.julioweb@gmail.com>
2020-06-29 00:49:55 -0400

    Primer commit To-Do con Reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+fetch('http://localhost:3004/me', {
+  mode: 'no-cors'
+})
+.then(res => res.json())
+.then(response => console.log(response));
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form action="">
+      <input type="text" placeholder='nombre'/>
+      <input type="email" placeholder='email'/>
+      <input type="password" placeholder='contraseña'/>
+    </form>
     </div>
   );
 }
